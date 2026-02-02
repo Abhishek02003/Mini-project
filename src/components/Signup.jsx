c359b49ef9c4585addd5f18db834dfec14a6eeba
Abhishek Agarwal <104927554+Abhishek02003@users.noreply.github.com>
2026-02-02 16:42:59 +0530

    Update signup API endpoint to production URL

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -16,7 +16,7 @@ const Signup = () => {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/signup", {
+      const res = await fetch("https://mini-project-backend-3g5a.onrender.com/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
